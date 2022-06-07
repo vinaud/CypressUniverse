6c543a533ff2937dd8822b62a7c27d4fca4c9f67
Vinaud <leyenasd@gmail.com>
2022-06-07 09:09:05 -0300

    Test: Input typing

--- training-wheels/cypress/e2e/login.cy.js
+++ training-wheels/cypress/e2e/login.cy.js
@@ -1,5 +1,7 @@
 describe('Login do usuário', () => {
   it('deve conseguir logar com sucesso', () => {
-    cy.visit('/login')
+    cy.visit('/login');
+    cy.get('#nickId').type('papitorocks');
+    cy.get('#passId').type('pwd123');
   })
 })

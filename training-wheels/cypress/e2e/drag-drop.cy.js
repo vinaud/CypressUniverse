d6382f09727f9999278fb9d34d5329add68e96c5
Vinaud <leyenasd@gmail.com>
2022-06-07 13:32:11 -0300

    Test: cypress hook

--- training-wheels/cypress/e2e/drag-drop.cy.js
+++ training-wheels/cypress/e2e/drag-drop.cy.js
@@ -1,21 +1,22 @@
 describe('teste de drag-drop', () => {
-  it('deve arrastar o cypress para aa caixinha do node', () => {
-    cy.visit('/drag_and_drop');
 
+  before(function(){
+    cy.visit('/drag_and_drop');
+  });
+
+  it('deve arrastar o cypress para aa caixinha do node', () => {
+    
     const dataTransfer = new DataTransfer();
 
     cy.get('img[alt="Cypress"]').trigger('dragstart', {dataTransfer});
-
     cy.get(' .nodejs figure[draggable="true"]').trigger('drop', {dataTransfer});
   });
 
   it('deve arrastar o robot para aa caixinha do python', () => {
-    cy.visit('/drag_and_drop');
 
     const dataTransfer = new DataTransfer();
 
     cy.get('img[alt="Robot Framework"]').trigger('dragstart', {dataTransfer});
-
     cy.get(' .python figure[draggable="true"]').trigger('drop', {dataTransfer});
   })
 })

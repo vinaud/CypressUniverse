ca41755fff0f4cea4f50a71e3ecb6e410dbff7bd
Vinaud <leyenasd@gmail.com>
2022-06-07 16:36:04 -0300

    Test: multiple select

--- training-wheels/cypress/e2e/select2.cy.js
+++ training-wheels/cypress/e2e/select2.cy.js
@@ -3,5 +3,20 @@ describe('test other selects', () => {
     cy.visit('/apps/select2/index.html');
     cy.get('.select2-selection--single').click();
     cy.contains('.select2-results__option', 'Cypress').click();
+  });
+
+  it('deve selecioanr multiplas opções', () => {
+    cy.visit('/apps/select2/index.html');
+
+    const frameworks = ['Cypress', 'Playwright', 'Protractor', 'Robot Framework'];
+
+    frameworks.forEach((framework) =>{
+
+      cy.get('.select2-selection--multiple').click();
+      cy.contains('.select2-results__option', framework).click();
+
+    });
+
+  
   })
 })

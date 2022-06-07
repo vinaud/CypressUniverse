41531e7dd02ec4e428ce7af8073a679799ca06b4
Vinaud <leyenasd@gmail.com>
2022-06-07 09:54:10 -0300

    Test: text validattion

--- training-wheels/cypress/e2e/login.cy.js
+++ training-wheels/cypress/e2e/login.cy.js
@@ -4,5 +4,10 @@ describe('Login do usuário', () => {
     cy.get('#nickId').type('papitorocks');
     cy.get('#passId').type('pwd123');
     cy.get('form .button[type=submit]').click();
+    cy.get('h3[class^="title').should('have.text', 'Olá Papito, bem-vindo ao Orkut');
+
+    const expectedText = 'Em breve você poderá participar de comunidades, adicionar amigos e deixar um Scraps. hahahahah';
+    cy.get('p.subheader').should('have.text', expectedText);
+
   })
 })

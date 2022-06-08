689970e14c3bcecbe9ec4560461c26401b8cb305
Vinaud <leyenasd@gmail.com>
2022-06-08 09:40:36 -0300

    Fix: correct datepicker test

--- training-wheels/cypress/e2e/datepicker.cy.js
+++ training-wheels/cypress/e2e/datepicker.cy.js
@@ -3,9 +3,9 @@ describe('teste datepicker', () => {
     cy.visit('/datepicker')
 
     const date = {
-      month: 'mar',
+      month: 'nov',
       year: '1983',
-      day: '28'
+      day: '30'
     }
 
     cy.get(' .datetimepicker-dummy-input').click();
@@ -16,8 +16,9 @@ describe('teste datepicker', () => {
     cy.get(' .datepicker-nav-year').click();
     cy.contains('.datepicker-year span', date.year).click();
 
-    cy.contains('.is-current-month button.date-item', date.day).click();
-    //cy.contains('button[class=date-item]', new RegExp("^" + date.day + "$", "g")).click()
+    cy.contains('.is-current-month button.date-item',  new RegExp("^" + date.day + "$", "g")).click();
+    //^ = begins with,  $ = ends with modifier, g -> exact comparison /^day$/g
+    
 
   })
 })

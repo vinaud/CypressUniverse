5167eb9f62c5cf8b09f9529b5481d7b2367a873b
Vinaud <leyenasd@gmail.com>
2022-06-09 09:14:33 -0300

    Test: more iframe tests

--- training-wheels/cypress/e2e/iframes.cy.js
+++ training-wheels/cypress/e2e/iframes.cy.js
@@ -7,10 +7,26 @@ describe('testes de iframes', () => {
     .then(cy.wrap);
   }
 
+  const badIFrame = function(){
+    return cy
+    .get('iframe[src*=instagram]')
+    .its('0.contentDocument.body').should('not.be.empty')
+    .then(cy.wrap);
+  }
+
   it('deve validar o total de seguidores', () => {
     cy.visit('/nice_iframe');
 
     niceIFrame().contains('.FollowerCountText','6,761 followers').should('be.visible');
+    niceIFrame().find('.UsernameText').should('have.text', 'qacademy_oficial');
+
+
+  });
+
+  it('deve validar o total de seguidores no iframe sem id', () => {
+    cy.visit('/bad_iframe');
+
+    badIFrame().contains('.FollowerCountText','6,761 followers').should('be.visible');
 
 
   })

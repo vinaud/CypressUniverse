2c960edfbcd8f0aa25592e1f0174ac70a4e550c4
Vinaud <leyenasd@gmail.com>
2022-06-09 11:38:01 -0300

    Test: confirm alert

--- training-wheels/cypress/e2e/alerts.cy.js
+++ training-wheels/cypress/e2e/alerts.cy.js
@@ -6,5 +6,30 @@ describe('teste de alertas', () => {
     cy.on('window:alert', function(result){
       expect(result).to.be.equal('Isto é uma mensagem de alerta')
     });
-  })
+
+    
+  });
+
+  it('deve confirmar a solicitaçãoa', () => {
+    cy.visit('/javascript_alerts');
+    cy.contains('button', 'Confirma').click();
+
+    cy.on('window:confirm',  () => true);
+
+    cy.get('#result').should('have.text', 'Mensagem confirmada');
+
+    
+  });
+
+  it('deve cancelar a solicitaçãoa', () => {
+    cy.visit('/javascript_alerts');
+    cy.contains('button', 'Confirma').click();
+
+    cy.on('window:confirm',  () => false);
+
+    cy.get('#result').should('have.text', 'Mensagem não confirmada');
+
+    
+  });
+
 })

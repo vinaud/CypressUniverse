0db4906241dffe1f11b6016caf4d8440b2e4f699
Vinaud <leyenasd@gmail.com>
2022-06-13 09:30:51 -0300

    Chore: Set defaul viewport

--- samuraibs/tests/cypress.config.js
+++ samuraibs/tests/cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 1440,
+    viewportHeight: 900
   },
 });

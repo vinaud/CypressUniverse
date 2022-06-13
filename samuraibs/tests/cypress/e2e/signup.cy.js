d5fb83a8769942a09b48fc841264718ca9ca3b56
Vinaud <leyenasd@gmail.com>
2022-06-13 14:44:52 -0300

    Test: scenario where user already exists

--- samuraibs/tests/cypress/e2e/signup.cy.js
+++ samuraibs/tests/cypress/e2e/signup.cy.js
@@ -32,5 +32,29 @@ describe('Teste de cadastro no sistema', () => {
       .should('be.visible')
       .find('p')
       .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
-  })
+  });
+
+  it('deve exibir email ja cadastrado', () => {
+
+    const user = {
+      name: 'João Vinaud',
+      email: 'vinaud@samuraibs.com',
+      password: 'pwd123'
+    }
+
+    cy.visit('/signup');
+
+    cy.get('input[placeholder="Nome"]').type(user.name);
+    cy.get('input[placeholder="E-mail"]').type(user.email);
+    cy.get('input[placeholder="Senha"]').type(user.password);
+
+    cy.contains('button', 'Cadastrar').click();
+
+    cy.get('.toast')
+      .should('be.visible')
+      .find('p')
+      .should('have.text', 'Email já cadastrado para outro usuário.');
+  });
+
+
 })

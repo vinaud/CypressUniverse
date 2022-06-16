97836b7b990db3f29d9a0ee1e8118ff0efe361da
Vinaud <leyenasd@gmail.com>
2022-06-16 11:20:49 -0300

    Update: Rebuil data test input

--- samuraibs/tests/cypress/e2e/login.cy.js
+++ samuraibs/tests/cypress/e2e/login.cy.js
@@ -1,16 +1,33 @@
 import loginPage from '../support/pages/login';
 import dashPage from '../support/pages/dashboard';
 
-describe('página de login', function(){
-    context('quando o usuário é muito bom', function(){
+describe('página de login', function () {
+    context('quando o usuário é muito bom', function () {
 
         const user = {
             name: 'Robson Jassa',
             email: 'jassa@samuraibs.com',
-            password: 'pwd123'
+            password: 'pwd123',
+            is_provider: true
         }
 
-        it('deve logar com sucesso', function(){
+        before(function () {
+
+            cy.task('removeUser', user.email).then(function (result) {
+                console.log(result);
+            });
+
+            cy.request(
+                'POST',
+                'http://localhost:3333/users',
+                user
+            ).then(function (response) {
+                expect(response.status).to.be.equal(200);
+            });
+
+        });
+
+        it('deve logar com sucesso', function () {
             loginPage.go();
             loginPage.form(user);
             loginPage.submit();

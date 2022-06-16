fa8c62bdb109c869edfa8afbd249c8fede82b2cf
Vinaud <leyenasd@gmail.com>
2022-06-16 14:56:13 -0300

    Test: verify login with incorrect password

--- samuraibs/tests/cypress/e2e/login.cy.js
+++ samuraibs/tests/cypress/e2e/login.cy.js
@@ -24,4 +24,30 @@ describe('página de login', function () {
             dashPage.header.userLoggedIn(user.name);
         });
     });
+
+    context('quando o usuário é bom, mas a senha está incorreta', function(){
+
+        let user = {
+            name: 'Celso Kamura',
+            email: 'kamura@samuraibs.com',
+            password: 'pwd123',
+            is_provider: true
+        }
+
+        before(function(){
+            cy.postUser(user).then(function(){
+                user.password = 'abc123';
+            });
+            
+        });
+
+        it('deve notificar erro de credenciais', function(){
+            loginPage.go();
+            loginPage.form(user);
+            loginPage.submit();
+
+            const expectedText = 'Ocorreu um erro ao fazer login, verifique suas credenciais.';
+            loginPage.toast.shouldHaveText(expectedText);
+        });
+    });
 });
--- samuraibs/tests/cypress/support/pages/login/index.js
+++ samuraibs/tests/cypress/support/pages/login/index.js
@@ -1,6 +1,11 @@
 import { el } from  './elements';
+import  toast  from '../components/toast'
 
 class LoginPage {
+
+    constructor(){
+        this.toast = toast;
+    }
 
     go(){
         cy.visit('/');
@@ -14,6 +19,7 @@ class LoginPage {
     submit(){
         cy.contains(el.signInButton).click();
     }
+
 }
 
 export default new LoginPage()

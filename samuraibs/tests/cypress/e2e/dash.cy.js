58414b51e79195f323654261267683dd280c199b
Vinaud <leyenasd@gmail.com>
2022-06-22 15:25:29 -0300

    Test: get provider Id via API

--- samuraibs/tests/cypress/e2e/dash.cy.js
+++ samuraibs/tests/cypress/e2e/dash.cy.js
@@ -20,12 +20,34 @@ describe('dashboard', function () {
             cy.postUser(data.provider);
             cy.postUser(data.customer);
             
-            cy.apiLogin(data.customer)
+            cy.apiLogin(data.customer);
+            cy.setProviderId(data.provider.email);
         })
 
         it('deve ser exibido no dashboard', function () {
-            console.log(data)
+            cy.log('Id do ramon é ' + Cypress.env('providerId'));
         });
+    });
+});
+
+Cypress.Commands.add('setProviderId', function(providerEmail){
+
+    cy.request({
+        method: 'GET',
+        url: 'http://localhost:3333/providers',
+        headers: {
+            authorization: 'Bearer ' + Cypress.env('apiToken')
+        }
+    }).then(function(response){
+        expect(response.status).to.eq(200);
+        cy.log(response.body);
+
+        const providerList = response.body;
+        providerList.forEach(function(provider){
+            if(provider.email == providerEmail){
+                Cypress.env('providerId', provider.id);
+            }
+        })
     });
 });
 

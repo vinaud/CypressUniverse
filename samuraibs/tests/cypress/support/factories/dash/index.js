2bbcc8d947da0bf932fa5ae576205f9bbd0decfb
Vinaud <leyenasd@gmail.com>
2022-06-27 11:05:59 -0300

    Test: Using underscore to generate dynamic test data

--- samuraibs/tests/cypress/support/factories/dash/index.js
+++ samuraibs/tests/cypress/support/factories/dash/index.js
@@ -1,3 +1,5 @@
+import _ from 'underscore';
+
 exports.customer = {
     name: 'Nikki Sixx',
     email: 'sixx@motleycrue.com',
@@ -13,6 +15,6 @@ exports.provider = {
 }
 
 exports.appointment = {
-    hour: '14:00'
+    hour: _.sample(['08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00',])
 }
 

335c9ce56e7c8aeecfcd47a8bdcbea4b931e09f2
Vinaud <leyenasd@gmail.com>
2022-07-01 12:01:08 -0300

    Build: Deploy web and api to heroku

---
+++ samuraibs/apps/web/build/precache-manifest.7d6f3828efa3ce522a606a524057d890.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee6e058c24887ea1be96cc39537a2e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af8885de3ab90b22d8c0",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "af8885de3ab90b22d8c0",
+    "url": "/static/js/2.13b1b37d.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.13b1b37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2271b3a8ff9c5c29b907",
+    "url": "/static/js/main.b774763d.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "05bd6345090870566f01353fe971316a",
+    "url": "/static/media/background-sign-in.05bd6345.png"
+  },
+  {
+    "revision": "d00be0b22b07cf3eb759f6fc32c7a36e",
+    "url": "/static/media/background-sign-up.d00be0b2.png"
+  },
+  {
+    "revision": "4a478307e8b4f74d2d6b9ffe31eecf75",
+    "url": "/static/media/logo.4a478307.svg"
+  }
+]);
